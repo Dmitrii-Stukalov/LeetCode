8e0f7c33aaff42d9988fefdf76a9738d17d045bb
Dmitrii-Stukalov <60853118+dmitrii-stukalov@users.noreply.github.com>
2022-07-22 20:24:16 +0300

    Solve 1995

---
+++ src/test/Solution1995Test.java
@@ -0,0 +1,29 @@
+package test;
+
+import code.Solution1995;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution1995Test {
+	Solution1995 solution = new Solution1995();
+
+	@Test
+	void firstTest() {
+		int[] nums = new int[]{1, 2, 3, 6};
+		assertEquals(1, solution.countQuadruplets(nums));
+	}
+
+	@Test
+	void secondTest() {
+		int[] nums = new int[]{3,3,6,4,5};
+		assertEquals(0, solution.countQuadruplets(nums));
+	}
+
+	@Test
+	void thirdTest() {
+		int[] nums = new int[]{1,1,1,3,5};
+		assertEquals(4, solution.countQuadruplets(nums));
+	}
+
+}
